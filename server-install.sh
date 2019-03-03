6e582ff6c83d20878600d960c9056e5245bb11ce
Leonardo Bitzki <ljbitzki@gmail.com>
2019-03-03 16:06:24 -0300

    Update server-install.sh

--- server-install.sh
+++ server-install.sh
@@ -1,15 +1,10 @@
 # Backup and versioning of services configuration files on Development/Testing/Acceptance/Production servers.
 #
 # Installation script for the "main" server with GitLab. (Change the FQDN variable to the Full Qualifyied Domain Name URL or - if you don't have DNS a name - the URL as http://IP.ADDRESS)
-FQDN="http://company.aaa.bb"
-# Create the "configkeeper" user with a random-strong password
-PASS=$( tr -cd '[:alnum:]' < /dev/urandom | fold -w20 | head -n1 )
-echo -e "The \"configkeeper\" user password is: \e[31m${PASS}\e[0m\nPress ENTER to continue..."
-read CONFIRM
-useradd -m -p "${PASS}" configkeeper
-# Add the "configkeeper" user to the group that can access the server through ssh.
+FQDN="http://configkeeper.dev.ufrgs.br"
+# Add the "git" user to the group that can access the server through ssh.
 # E.g.: If in your sshd_config only members of "ssh-users" group can connnect via ssh, run:
-### usermod -aG ssh-users configkeeper
+usermod -aG ssh-users git
 # Root keypair 
 ssh-keygen -o -t rsa -b 4096
 # Update/Dist-Upgrade
